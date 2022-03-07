65035d70fcbaf50074b11cc12288af54c82e0065
wendy02062001 <wendy262001@gmail.com>
2022-03-07 15:08:46 +0700

    Final version

---
+++ src/AppThuKinh/AppThuKinh.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import GlassesList from "./GlassesList";
+
+export default class AppThuKinh extends Component {
+  render() {
+    return (
+      <div>
+        <GlassesList />
+      </div>
+    );
+  }
+}
---
+++ src/AppThuKinh/GlassesList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import GlassesItem from "./GlassesItem";
+
+export default class GlassesList extends Component {
+  render() {
+    return (
+      <div>
+        <GlassesItem />
+      </div>
+    );
+  }
+}
