2c97cccd949fccb85359dded8ea00b57816f1247
wendy02062001 <wendy262001@gmail.com>
2022-03-08 21:30:44 +0700

    Can try Glasses

--- src/AppThuKinh/GlassesItem.js
+++ src/AppThuKinh/GlassesItem.js
@@ -1,8 +1,32 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class GlassesItem extends Component {
+class GlassesItem extends Component {
   render() {
     let { glasses } = this.props;
-    return <img className="w-75 h-100" style={{objectFit:'contain'}} src={"./img/g" + glasses.id + ".jpg"}></img>;
+    return (
+      <img
+        className="w-75 h-100"
+        style={{ objectFit: "contain" }}
+        src={"./img/g" + glasses.id + ".jpg"}
+        onClick={() => {
+          this.props.tryGlasses(glasses);
+        }}
+      ></img>
+    );
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    tryGlasses: (glasses) => {
+      const action = {
+        type: "TRY_GLASSES",
+        glasses,
+      };
+      dispatch(action);
+    },
+  };
+};
+
+export default connect(null, mapDispatchToProps)(GlassesItem);
--- src/redux/AppThuKinhReducer.js
+++ src/redux/AppThuKinhReducer.js
@@ -9,6 +9,10 @@ const stateDefault = {
 
 export const appThuKinhReducer = (state = stateDefault, action) => {
   switch (action.type) {
+    case "TRY_GLASSES":
+      state.glassOnTry = { ...action.glasses };
+      return { ...state };
+
     default:
       return state;
   }
